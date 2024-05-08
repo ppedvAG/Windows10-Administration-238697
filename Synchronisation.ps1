f08d502894e941f0662dedb1abfef769cc41b45f
DanielR <113525395+Shimotakii@users.noreply.github.com>
2024-05-08 14:42:11 +0200

    Sync

---
+++ Synchronisation.ps1
@@ -0,0 +1,17 @@
+﻿# Pfade definieren
+$quellePfad = ""
+$zielPfad = ""
+
+# Überprüfen, ob der Quellpfad existiert
+if (-not (Test-Path -Path $quellePfad)) {
+    Write-Host Quellpfad existiert nicht $quellePfad
+    exit
+}
+
+# Überprüfen, ob der Zielpfad existiert, falls nicht, erstelle ihn
+if (-not (Test-Path -Path $zielPfad)) {
+    New-Item -Path $zielPfad -ItemType Directory -Force
+}
+
+# Alle Dateien und Unterverzeichnisse von der Quelle zum Ziel kopieren
+Copy-Item -Path $quellePfad -Destination $zielPfad -Recurse -Force -Verbose
